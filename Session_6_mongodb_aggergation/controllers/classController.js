740cc0d7e32151a125b8ea066d0ffcaf7409b880
khanhlong2008 <khanhlong11a1@mgail.com>
2021-11-18 19:44:46 +0700

    ss10

--- Session_6_mongodb_aggergation/controllers/classController.js
+++ Session_6_mongodb_aggergation/controllers/classController.js
@@ -38,3 +38,62 @@ const getAllclassesWithTeacher = async () => {
 };
 
 module.exports = { getAllclassesWithTeacher };
+/*
+*giống hàm fitted là lọc dữ liệu
+$match: {
+            vd: {
+                    teacher_id:"a"
+                }
+            $in a hoặc b  
+            vd: {
+                    teacher_id:{
+                        $in:['a','b']
+                    }
+                }
+        }
+
+*Thêm các trường mới vào dữ liệu         
+$addfieds: {
+    $$NOW: NGÀY THÁNG TRONG JAVASCIPT
+    now: "$$NOW"
+}        
+
+*lấy phần tử đầu tiên của dữ liệu 
+$limit:{
+    1
+}
+
+*nhảy qua một bước nhảy lấy phần tử tiếp theo chạy từ 1 đến n 
+$skip:{
+    1
+}
+
+*Sắp xếp các trường mong muốn 
+$sort:{
+    teacher_id:'a'
+}
+
+*chọn ra các trường dữ liệu được trả ra 
+$project:{
+    name:1,
+    _id:0,
+    teacher_id:1,
+}
+
+*cho phép chọn ra 4 key 
+    from: tên collection mong muốn (bảng),
+    localField: trường mà trùng giữ cac bảng ,
+    id == localField,
+    as: kết quả lấy ra xong rồi thì lưu vào nó và có thể đổi tên 
+$lookup:{
+    from:
+    localField: 
+    id:
+    as:
+}
+
+*giúp chúng ta trả ra các phần tử của trường mà chúng ta điền vào path
+$unwind:{
+    path:'$taecher$
+}
+*/
